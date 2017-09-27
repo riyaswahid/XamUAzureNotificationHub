ebc1bf8964b90f91fa6139862caa9580e770e210
René Ruppert <rene.ruppert@gmail.com>
2017-09-27 17:40:31 +0200

    fix

--- XamUNotif/XamUNotif.Android/AndroidMessageReceiver.cs
+++ XamUNotif/XamUNotif.Android/AndroidMessageReceiver.cs
@@ -1,28 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using XamUNotif;
-<<<<<<< HEAD
-
-namespace XamUNotif.Droid
-{
-=======
 using Xamarin.Forms;
 using XamUNotif.Droid;
 
 [assembly: Dependency(typeof(AndroidMessageReceiver))]
 namespace XamUNotif.Droid
 {
-	
->>>>>>> refs/heads/test-android
 	public class AndroidMessageReceiver : IMessageReceiver
 	{
 		public event EventHandler<string> MessageReceived;
