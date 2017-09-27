3e53fa8e8f787eb91a0bb4b52e908bd7f8fc0e16
René Ruppert <rene.ruppert@gmail.com>
2017-09-27 20:17:21 +0200

    Add all parts

--- XamUNotif/XamUNotif.Android/AndroidMessageReceiver.cs
+++ XamUNotif/XamUNotif.Android/AndroidMessageReceiver.cs
@@ -8,5 +8,10 @@ namespace XamUNotif.Droid
 	public class AndroidMessageReceiver : IMessageReceiver
 	{
 		public event EventHandler<string> MessageReceived;
+
+		internal void Handle(string msg)
+		{
+			MessageReceived?.Invoke(this, msg);
+		}
 	}
 }
--- XamUNotif/XamUNotif.Android/MainActivity.cs
+++ XamUNotif/XamUNotif.Android/MainActivity.cs
@@ -6,6 +6,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
+using Android.Gms.Common;
+using Xamarin.Forms;
 
 namespace XamUNotif.Droid
 {
@@ -21,6 +26,85 @@ namespace XamUNotif.Droid
 
 			global::Xamarin.Forms.Forms.Init(this, bundle);
 			LoadApplication(new App());
+
+			// In a "real" app you will have to deal with it if services are unavailable!
+			IsPlayServicesAvailable();
+
+
+		}
+
+		public bool IsPlayServicesAvailable()
+		{
+			int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+			if (resultCode != ConnectionResult.Success)
+			{
+				if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+				{
+					// In a real project you can give the user a chance to fix the issue.
+					Console.WriteLine($"Error: {GoogleApiAvailability.Instance.GetErrorString(resultCode)}");
+				}
+				else
+				{
+					Console.WriteLine("Error: Play services not supported!");
+					Finish();
+				}
+				return false;
+			}
+			else
+			{
+				Console.WriteLine("Play Services available.");
+				return true;
+			}
+		}
+	}
+
+	// This service handles the device's registration with FCM.
+	[Service]
+	[IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+	public class MyFirebaseIIDService : FirebaseInstanceIdService
+	{
+		public override void OnTokenRefresh()
+		{
+			var refreshedToken = FirebaseInstanceId.Instance.Token;
+			Console.WriteLine($"Token received: {refreshedToken}");
+			SendRegistrationToServer(refreshedToken);
+		}
+
+		void SendRegistrationToServer(string token)
+		{
+			// We'll do this later :-)
+		}
+	}
+
+	// This service is used if app is in the foreground and a message is received.
+	[Service]
+	[IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+	public class MyFirebaseMessagingService : FirebaseMessagingService
+	{
+		public override void OnMessageReceived(RemoteMessage message)
+		{
+			base.OnMessageReceived(message);
+
+			Console.WriteLine("Received: " + message);
+
+			// Android supports different message payloads. To use the code below it must be something like this (you can paste this into Azure test send window):
+			// {
+			//   "notification" : {
+			//      "body" : "The body",
+			//                 "title" : "The title",
+			//                 "icon" : "myicon
+			//   }
+			// }
+			try
+			{
+				var msg = message.GetNotification().Body;
+				var msgReceiver = (AndroidMessageReceiver)DependencyService.Get<IMessageReceiver>(DependencyFetchTarget.GlobalInstance);
+				msgReceiver.Handle(msg);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error extracting message: " + ex);
+			}
 		}
 	}
 }
